cb2a0125e41c716802b6a8f3541094471fba27ff
Arpitha Gangatkar <arpitha.gangatkar@gmail.com>
2023-06-01 17:36:27 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name='RegressorProject',
+    version='0.0.1',
+    author='arpitha',
+    author_email='arpitha.gangatkar@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
+
+# to run individually "python setup.py install"
